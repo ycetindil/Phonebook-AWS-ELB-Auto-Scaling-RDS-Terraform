f9c297f37215324a943f4cb884dcd5f7454a8e66
Yasin Cetindil <ycetindil@gmail.com>
2023-02-23 22:30:55 -0800

    Minor format fixes

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ data "aws_vpc" "default_vpc" {
 data "aws_subnets" "subnets" {
   filter {
     name   = "vpc-id"
-    values = [data.aws_vpc.default_vpc.id]
+    values = [ data.aws_vpc.default_vpc.id ]
   }
 }
 
@@ -68,14 +68,14 @@ resource "aws_security_group" "db_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [ "0.0.0.0/0" ]
   }
 }
 
 resource "aws_db_instance" "db_server" {
   instance_class              = "db.t2.micro"
   allocated_storage           = 20 # GB
-  vpc_security_group_ids      = [aws_security_group.db_sg.id]
+  vpc_security_group_ids      = [ aws_security_group.db_sg.id ]
   allow_major_version_upgrade = false
   auto_minor_version_upgrade  = true
   backup_retention_period     = 0
@@ -96,12 +96,12 @@ resource "aws_db_instance" "db_server" {
 # Launch Template #
 ###################
 data "aws_ami" "amazon_linux_2" {
-  owners      = ["amazon"]
+  owners      = [ "amazon" ]
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10*"]
+    values = [ "amzn2-ami-kernel-5.10*" ]
   }
 }
 
@@ -116,7 +116,7 @@ resource "aws_security_group" "lt_sg" {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
+    security_groups = [ aws_security_group.alb_sg.id ]
   }
 
   ingress {
@@ -141,7 +141,9 @@ resource "aws_launch_template" "lt" {
   key_name               = var.ssh_key_name
   vpc_security_group_ids = [aws_security_group.lt_sg.id]
   user_data              = filebase64("user-data.sh")
-  depends_on             = [github_repository_file.dbendpoint]
+  depends_on             = [
+    github_repository_file.dbendpoint
+  ]
 
   tag_specifications {
     resource_type = "instance"
@@ -178,14 +180,14 @@ resource "aws_security_group" "alb_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [ "0.0.0.0/0" ]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [ "0.0.0.0/0" ]
   }
 }
 
@@ -205,7 +207,7 @@ resource "aws_alb" "alb" {
   ip_address_type    = "ipv4"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
+  security_groups    = [ aws_security_group.alb_sg.id ]
   subnets            = data.aws_subnets.subnets.ids
 }
 
@@ -219,7 +221,7 @@ resource "aws_autoscaling_group" "asg" {
   name                      = "${var.prefix}-asg"
   health_check_grace_period = 300
   health_check_type         = "ELB"
-  target_group_arns         = [aws_alb_target_group.alb_tg.arn]
+  target_group_arns         = [ aws_alb_target_group.alb_tg.arn ]
   vpc_zone_identifier       = aws_alb.alb.subnets
 
   launch_template {
